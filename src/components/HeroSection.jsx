2c4bb1df17404724976911ca66281668caaf1569
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-02 23:34:35 -0400

    got hero section mostly done. Next is About me section.

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,5 +1,5 @@
 
-
+import { ArrowDown } from "lucide-react";
 
 export const HeroSection = () => {
     return (
@@ -15,8 +15,25 @@ export const HeroSection = () => {
                         <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2"> Patel</span>
                     </h1>
 
+                    <p className="text-lg md:text-xl text-muted-foreground mx-auto max-2-2xl opacity-0 animate-fade-in-delay-3"> 
+                        Student Software Engineer
+                    </p>
+
+                    <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                        <a href="#projects" className="cosmic-button">
+                            View my Work
+                        </a>
+                    </div>
+
                 </div>
             </div> 
+
+           <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce ">
+                <span className="text-sm text-muted-foreground mb-2"> Scroll</span>
+                <ArrowDown className="h-5 w-5 text-primary"></ArrowDown>
+           </div> 
         </section>
     );
 };
+ // got the hero section working, now need to add the projects section and footer
+ //video at 1.28.30
