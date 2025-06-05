c6c5d26662f0d3a25866e469ce1be92e8c39ce36
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-04 21:56:20 -0400

    added basic grid and filler text

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -5,9 +5,27 @@ export const AboutSection = () => {
     return (
     <section id="about" className="py-24 px-4 relative"> 
         <div className="container mx-auto max-2-5xl">
-            <h2>
+            <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                 About <span className="text-primary"> Me</span>
             </h2>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 gap-12 items-center">
+                <div className="space-y-6">
+                    <h3>Passionate Web Developer</h3>
+
+                    <p className="text-muted-foreground">
+                        Studying over 6 yeasrs at njit. blah blah blah
+                        inserting
+                        rand om phrase 
+                    </p>
+                    <p className="text-muted-foreground">
+                        Just having some insert                    
+                    </p>
+                </div>
+                <div>
+
+                </div>
+            </div>
         </div>
     </section>);
 };
