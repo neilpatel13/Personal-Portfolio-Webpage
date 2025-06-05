68e5a20f0760add968c7668cd0fd6015657fd83f
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-04 20:16:42 -0400

    started about me section

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,13 @@
+
+
+
+export const AboutSection = () => {
+    return (
+    <section id="about" className="py-24 px-4 relative"> 
+        <div className="container mx-auto max-2-5xl">
+            <h2>
+                About <span className="text-primary"> Me</span>
+            </h2>
+        </div>
+    </section>);
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
 
 
 export const Home = () => {
@@ -18,6 +19,7 @@ export const Home = () => {
             {/* Main Content */}
             <main>
                 <HeroSection />
+                <AboutSection />
             </main>
             {/* Footer */}
         </div>
