af5c4cec8f1ff456c7044ba74ee6f60f96ca4495
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-05 23:33:59 -0400

    button for choosing which skillset

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { cn } from "@/lib/utils";
 
 const skills =  [
     // Frontend
@@ -40,7 +40,19 @@ export const SkillsSection = () => {
                 </h2>
 
                 <div className="flex felx-wrap justify-center gap-4 mb-12">
-
+                    {categories.map((category, key) => (
+                        <button 
+                            key={key} 
+                            onClick={() => setActiveCategory(category)}
+                            className={cn(
+                                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                                 activeCategory === category ? "bg-primary text-primary-foreground" : 
+                                 "bg-secondary/70 text-foreground hover:bg-secondary"
+                            )}
+                        >
+                            {category}
+                        </button>
+                    ))}
 
                 </div>
 
