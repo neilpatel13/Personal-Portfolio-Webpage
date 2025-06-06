9b26377280769ae048526febb1330554536a736a
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-05 23:07:40 -0400

    skills being displayed

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -27,6 +27,31 @@ const skills =  [
 
 
 export const SkillsSection = () => {
-    return <section></section>
+    return (
+        <section 
+            id="skills" 
+            className="py-24 px-4 relative bg-secondary/30"
+        >
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    My <span className="text-primary"> Skills</span>
+                </h2>
 
+                <div className="grid gird-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {skills.map((skill, key) => (
+                        <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover">
+                            <div> 
+                                <h3> {skill.name}</h3>
+                            </div>
+
+                        </div>
+                    ))}
+
+                </div>
+
+            </div>
+
+
+        </section>
+    );
 };
