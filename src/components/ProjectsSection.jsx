2f285e30e102c41400b9d4f1567f71a031961e2b
Neil Patel <90282158+neilpatel13@users.noreply.github.com>
2025-06-08 19:23:59 -0400

    code cleanup

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -63,18 +63,18 @@ export const ProjectsSection = () => {
               <div className="p-6">
                 <div className="flex flex-wrap gap-2 mb-4">
                   {project.tags.map((tag) => (
-                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
+                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-primary/20 text-secondary-foreground">
                       {tag}
                     </span>
                   ))}
                 </div>
 
                 <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
-                <p className="text-muted-foreground text-sm mb-4">
+                <p className="text-muted-foreground text-sm mb-6">
                   {project.description}
                 </p>
-                <div className="flex justify-between items-center">
-                  <div className="flex space-x-3">
+                <div>
+                  <div className="flex justify-center items-center space-x-3">
                     <a
                       href={project.demoUrl}
                       target="_blank"
@@ -100,7 +100,7 @@ export const ProjectsSection = () => {
           <a
             className="cosmic-button w-fit flex items-center mx-auto gap-2"
             target="_blank"
-            href="https://github.com/machadop1407"
+            href="https://github.com/neilpatel13"
           >
             Check My Github <ArrowRight size={16} />
           </a>
